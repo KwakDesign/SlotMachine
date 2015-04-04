b87a94b0873ec1741a5db66681cd0c924422f49d
Abraham <kwak.abe@gmail.com>
2015-04-04 15:14:22 -0400

    began adding UIView

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstContainer: UIView!
+    var secondContainer: UIView!
+    var thirdContainer: UIView!
+    var fourthContainer: UIView!
+    
+    let kMarginForView: CGFloat = 10.0
+    let kSixth: CGFloat = 1.0/6.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setUpContainerViews()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func setUpContainerViews() {
+        self.firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.view.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+        self.firstContainer.backgroundColor = UIColor.redColor()
+        self.view.addSubview(firstContainer)
+    }
 
 }
 
